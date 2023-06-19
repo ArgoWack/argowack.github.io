3c3dbc2cb94643a52cfd903f8e8551e75addcfba
ArgoWack <45313830+ArgoWack@users.noreply.github.com>
2023-06-19 21:09:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,57 @@
 function initI18n() {
-  i18next
-    .use(i18nextFetchBackend)
-    .init({
-      lng: 'en', // default language
-      debug: true,
-      backend: {
-        loadPath: 'locales/{{lng}}.json',
-        requestOptions: {
-          mode: 'no-cors',
-        },
+  i18next.init({
+    lng: 'en', // default language
+    debug: true,
+    resources: {
+      en: {
+        translation: {}
       },
-    }, function(err, t) {
+      pl: {
+        translation: {}
+      }
+    }
+  }, function(err, t) {
+    if (err) {
+      console.error('Error initializing i18next:', err);
+      return;
+    }
+
+    // Load translations for "en" language
+    fetchTranslation('en').then(function(translations) {
+      i18next.addResources('en', 'translation', translations);
       updateContent();
+    }).catch(function(error) {
+      console.error('Error loading "en" translations:', error);
     });
+
+    // Load translations for "pl" language
+    fetchTranslation('pl').then(function(translations) {
+      i18next.addResources('pl', 'translation', translations);
+      updateContent();
+    }).catch(function(error) {
+      console.error('Error loading "pl" translations:', error);
+    });
+  });
+}
+
+function fetchTranslation(language) {
+  return fetch('locales/' + language + '.json')
+    .then(function(response) {
+      if (!response.ok) {
+        throw new Error('HTTP error ' + response.status);
+      }
+      return response.json();
+    });
+}
+
+function updateContent() {
+  document.querySelector('h1').innerHTML = i18next.t('welcome_title');
+  document.querySelector('p').innerHTML = i18next.t('welcome_message');
 }
 
 function changeLanguage(lng) {
   i18next.changeLanguage(lng, function(err, t) {
     updateContent();
   });
-}
-
-function updateContent() {
-  document.querySelector('h1').innerHTML = i18next.t('welcome_title');
-  document.querySelector('p').innerHTML = i18next.t('welcome_message');
 }
 
